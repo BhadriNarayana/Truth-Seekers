7529918973540ef0c16a50cc13eb27d3d159edb1
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-04-24 07:01:10 +0530

    Rendering rooms in our room.html

--- base/views.py
+++ base/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+rooms = [
+    {'id': 1, 'name': "Advaita Vedanta" },
+    {'id': 2, 'name': "Vishwadvaita Vedanta" },
+    {'id': 3, 'name': "Kriya Yoga" },
+]
+
 def home(request):
     return render(request, 'home.html')
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'room.html', {'rooms': rooms})
