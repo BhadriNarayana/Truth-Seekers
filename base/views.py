ae6cab2b4f6e0de55e96fffac455597c95888542
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-04-25 15:49:14 +0530

    Dynamic routing added to room according to room index

--- base/views.py
+++ base/views.py
@@ -13,5 +13,13 @@ def home(request):
     return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def room(request):
-    return render(request, 'base/room.html', {'rooms': rooms})
+def room(request, pk):
+    
+    room = None
+    
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            
+    
+    return render(request, 'base/room.html', {'room': room})
